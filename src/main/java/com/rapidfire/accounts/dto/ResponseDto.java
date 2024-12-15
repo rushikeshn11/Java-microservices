82a66875a8dbf948246b05b5298b64cb4f516dcb
rushikeshn11 <rushikeshnagothanekar@gmail.com>
2024-12-16 00:32:16 +0530

    Changes in Dto classes

--- src/main/java/com/rapidfire/accounts/dto/ResponseDto.java
+++ src/main/java/com/rapidfire/accounts/dto/ResponseDto.java
@@ -1,4 +1,13 @@
 package com.rapidfire.accounts.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
 public class ResponseDto {
+
+    private String statusCode;
+
+    private String statusMsg;
+
 }
